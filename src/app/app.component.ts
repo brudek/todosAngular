7c265ce7030dd7a020258ad9fd7debdf1e4ea9de
Arek Bru <brudek@op.pl>
2018-08-22 17:23:52 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  newTask: string;
+  tasksList: Array<string> = [];
+  tasksDone: Array<string> = [];
+
+  add() {
+    this.tasksList.push(this.newTask);
+    console.log(this.tasksList);
+    this.newTask = '';
+  }
+
+  remove(task: string) {
+    this.tasksList = this.tasksList.filter( e => e !== task);
+  }
+
+  done(task: string) {
+    this.tasksDone.push(task);
+    this.remove(task);
+  }
 }
